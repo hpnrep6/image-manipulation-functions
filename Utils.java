00525e886802a258632a844063ba6f0ee00ef1c0
hpnrep6 <57055412+hpnrep6@users.noreply.github.com>
2020-12-11 23:30:50 -0500

    Create Utils.java

---
+++ Utils.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
+
+public class Utils {
+    /**
+     * From https://stackoverflow.com/questions/3514158/how-do-you-clone-a-bufferedimage
+     *
+     * @params bi BufferedImage object to copy
+     *
+     * @return Copied BufferedImage
+     */
+    public static BufferedImage deepCopy(BufferedImage bi) {
+        ColorModel cm = bi.getColorModel();
+        boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+        WritableRaster raster = bi.copyData(bi.getRaster().createCompatibleWritableRaster());
+        return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
+    }
+    
+        /**
+     * Clamps a number between two values
+     *
+     * @param value Number to clamp
+     * @param min Minimum value
+     * @param max Maximum value
+     * @return Clamped value
+     */
+    public static int clamp(int value, int min, int max) {
+        return Math.min(Math.max(value, min), max);
+    }
+
+    /**
+     * Clamps a number between two values
+     *
+     * @param value Number to clamp
+     * @param min Minimum value
+     * @param max Maximum value
+     * @return Clamped value
+     */
+    public static float clamp(float value, float min, float max) {
+        return Math.min(Math.max(value, min), max);
+    }
+}
